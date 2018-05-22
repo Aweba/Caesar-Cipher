fb77ef765868dad84fd1438c073784c13e13bf98
Aweba13 <fiorella.effio@unmsm.edu.pe>
2018-05-22 11:39:07 -0500

    Tercera version offset variable

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var answer = parseInt(prompt("¿Que es lo que deseas hacer? \n 1.Cifrar \n 2.Descifrar"));
-var word = prompt("Ingrese el texto");
+var word = prompt("Ingrese el texto:");
+var offset = parseInt(prompt("Ingrese la cantidad de espacios:"));
 var array = word.split("");
 var cipher_word = "";
 var decipher_word = "";
@@ -14,11 +15,11 @@ function cipher(){
       {cipher_word = cipher_word + " ";}
     if(num_letra>=65 && num_letra<=90)
       {num = 65;
-       cipher_word = cipher_word + String.fromCharCode((array[i].charCodeAt(0) - num + 7) % 26 + num);
+       cipher_word = cipher_word + String.fromCharCode((array[i].charCodeAt(0) - num + offset) % 26 + num);
       }
     if(num_letra>=97 && num_letra<=122)
       {num = 97;
-    cipher_word = cipher_word + String.fromCharCode((array[i].charCodeAt(0) - num + 7) % 26 + num);}
+    cipher_word = cipher_word + String.fromCharCode((array[i].charCodeAt(0) - num + offset) % 26 + num);}
   }
   document.write(cipher_word);
 }
@@ -30,22 +31,22 @@ function decipher(){
       {decipher_word = decipher_word + " ";}
     if(num_letra>=65 && num_letra<=90)
       {num = 65;
-        var condition = (array[i].charCodeAt(0)-num) - 7;
+        var condition = (array[i].charCodeAt(0)-num) - offset;
         if(condition < 0){
-            decipher_word = decipher_word + String.fromCharCode((array[i].charCodeAt(0) +26 - 7));
+            decipher_word = decipher_word + String.fromCharCode((array[i].charCodeAt(0) +26 - offset));
         }
         else{
-            decipher_word = decipher_word + String.fromCharCode((array[i].charCodeAt(0) - num - 7 ) % 26 + num);
+            decipher_word = decipher_word + String.fromCharCode((array[i].charCodeAt(0) - num - offset ) % 26 + num);
         }
       }
     if(num_letra>=97 && num_letra<=122)
       {num = 97;
-        var condition = (array[i].charCodeAt(0)-num) - 7;
+        var condition = (array[i].charCodeAt(0)-num) - offset;
         if(condition < 0){
-            decipher_word = decipher_word + String.fromCharCode((array[i].charCodeAt(0) +26 - 7));
+            decipher_word = decipher_word + String.fromCharCode((array[i].charCodeAt(0) +26 - offset));
         }
         else{
-            decipher_word = decipher_word + String.fromCharCode((array[i].charCodeAt(0) - num - 7 ) % 26 + num);
+            decipher_word = decipher_word + String.fromCharCode((array[i].charCodeAt(0) - num - offset ) % 26 + num);
         }
       }
 
