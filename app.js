47524612cc43f429bda7d978465a4733c3753b64
Aweba13 <fiorella.effio@unmsm.edu.pe>
2018-05-22 10:50:58 -0500

    Segunda version con espacio en blanco y minusculas

--- app.js
+++ app.js
@@ -3,31 +3,59 @@ var word = prompt("Ingrese el texto");
 var array = word.split("");
 var cipher_word = "";
 var decipher_word = "";
+var num;
+
 var count = word.length-1;
 var i;
 function cipher(){
   for(i=0;i<=count;i++) {
-    cipher_word = cipher_word + String.fromCharCode((array[i].charCodeAt(0) - 65 + 7) % 26 + 65);
+    var num_letra = array[i].charCodeAt(0);
+    if(array[i] === " ")
+      {cipher_word = cipher_word + " ";}
+    if(num_letra>=65 && num_letra<=90)
+      {num = 65;
+       cipher_word = cipher_word + String.fromCharCode((array[i].charCodeAt(0) - num + 7) % 26 + num);
+      }
+    if(num_letra>=97 && num_letra<=122)
+      {num = 97;
+    cipher_word = cipher_word + String.fromCharCode((array[i].charCodeAt(0) - num + 7) % 26 + num);}
   }
   document.write(cipher_word);
 }
 function decipher(){
-  for(i=0;i<=count;i++) {
-    var condition = (array[i].charCodeAt(0)-65) - 7;
-    if(condition < 0){
-        decipher_word = decipher_word + String.fromCharCode((array[i].charCodeAt(0) +26 - 7));
-    }
-    else{
-        decipher_word = decipher_word + String.fromCharCode((array[i].charCodeAt(0)-65 - 7 ) % 26 + 65);
 
-    }
+  for(i=0;i<=count;i++) {
+    var num_letra = array[i].charCodeAt(0);
+    if(array[i] === " ")
+      {decipher_word = decipher_word + " ";}
+    if(num_letra>=65 && num_letra<=90)
+      {num = 65;
+        var condition = (array[i].charCodeAt(0)-num) - 7;
+        if(condition < 0){
+            decipher_word = decipher_word + String.fromCharCode((array[i].charCodeAt(0) +26 - 7));
+        }
+        else{
+            decipher_word = decipher_word + String.fromCharCode((array[i].charCodeAt(0) - num - 7 ) % 26 + num);
+        }
+      }
+    if(num_letra>=97 && num_letra<=122)
+      {num = 97;
+        var condition = (array[i].charCodeAt(0)-num) - 7;
+        if(condition < 0){
+            decipher_word = decipher_word + String.fromCharCode((array[i].charCodeAt(0) +26 - 7));
+        }
+        else{
+            decipher_word = decipher_word + String.fromCharCode((array[i].charCodeAt(0) - num - 7 ) % 26 + num);
+        }
+      }
+
 }
     document.write(decipher_word);
 }
 
 if(answer === 1){
-  cipher();
+     cipher();
 }
 if(answer === 2){
-  decipher();
+     decipher();
 }
